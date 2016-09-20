d06eebe1d8c91512df33ab68d9d80cb6f0cc07c6
Tino Rusch <tino.rusch@webvariants.de>
2016-09-20 14:11:13 +0200

    added license headers;

--- include/susi/LevelDBComponent.h
+++ include/susi/LevelDBComponent.h
@@ -1,28 +1,38 @@
+/*
+ * Copyright (c) 2016, Tino Rusch
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author: Tino Rusch (tino.rusch@webvariants.de)
+ */
+
 #include "leveldb/db.h"
 #include "susi/SusiClient.h"
 
 namespace Susi {
-	class LevelDBComponent {
+class LevelDBComponent {
 
-	public:
-		LevelDBComponent(Susi::SusiClient & susi, std::string dbPath);
-		void join();
+public:
+  LevelDBComponent(Susi::SusiClient &susi, std::string dbPath);
+  void join();
 
-	protected:
-		Susi::SusiClient & susi_;
-		leveldb::Options dbOptions_;
-		std::string dbPath_;
-		std::shared_ptr<leveldb::DB> db_;
+protected:
+  Susi::SusiClient &susi_;
+  leveldb::Options dbOptions_;
+  std::string dbPath_;
+  std::shared_ptr<leveldb::DB> db_;
 
-		bool validateFieldsForPut(const Susi::EventPtr & event);
-		bool validateFieldsForGet(const Susi::EventPtr & event);
-		bool validateFieldsForGetRange(const Susi::EventPtr & event);
+  bool validateFieldsForPut(const Susi::EventPtr &event);
+  bool validateFieldsForGet(const Susi::EventPtr &event);
+  bool validateFieldsForGetRange(const Susi::EventPtr &event);
 
-		void handlePut(Susi::EventPtr event);
-		void handleGet(Susi::EventPtr event);
-		void handleDelete(Susi::EventPtr event);
-		void handleGetPrefix(Susi::EventPtr event);
-		void handleGetRange(Susi::EventPtr event);
-
-	};
+  void handlePut(Susi::EventPtr event);
+  void handleGet(Susi::EventPtr event);
+  void handleDelete(Susi::EventPtr event);
+  void handleGetPrefix(Susi::EventPtr event);
+  void handleGetRange(Susi::EventPtr event);
+};
 }
--- main.cpp
+++ main.cpp
@@ -1,25 +1,38 @@
+/*
+ * Copyright (c) 2016, Tino Rusch
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author: Tino Rusch (tino.rusch@webvariants.de)
+ */
+
 #include "susi/LevelDBComponent.h"
 #include "susi/BaseApp.h"
 
 class LevelDBApp : public Susi::BaseApp {
 protected:
-    std::shared_ptr<Susi::LevelDBComponent> _leveldbComponent;
+  std::shared_ptr<Susi::LevelDBComponent> _leveldbComponent;
+
 public:
-    LevelDBApp(int argc, char **argv) : Susi::BaseApp{argc,argv} {}
-    virtual ~LevelDBApp() {}
-    virtual void start() override {
-        _leveldbComponent.reset(new Susi::LevelDBComponent{*_susi,_config["component"]["db"]});
-    }
+  LevelDBApp(int argc, char **argv) : Susi::BaseApp{argc, argv} {}
+  virtual ~LevelDBApp() {}
+  virtual void start() override {
+    _leveldbComponent.reset(
+        new Susi::LevelDBComponent{*_susi, _config["component"]["db"]});
+  }
 };
 
-int main(int argc, char *argv[]){
-    try{
-        LevelDBApp app{argc,argv};
-        app.start();
-        app.join();
-    }catch(const std::exception & e){
-        std::cout << e.what() << std::endl;
-        return 1;
-    }
-    return 0;
+int main(int argc, char *argv[]) {
+  try {
+    LevelDBApp app{argc, argv};
+    app.start();
+    app.join();
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
 }
